c6f004aff5e6649723208c56a02f1e820c02b7db
centdollar <vincent.michelini1@gmail.com>
2022-09-29 16:44:35 -0400

    began the start of the shift module

---
+++ shift_reg/shift_reg.v
@@ -0,0 +1,18 @@
+// Shift register
+// Author: Vincent Michelini
+// Description: intended to make a shift register with a lot of modular
+// functionality and paramaterization to make this functionality possible
+//
+// TODO: first get a basic shift register made.
+
+
+module shift_reg
+#(
+
+)
+(
+
+    input [7 : 0] din_i,
+
+
+);
