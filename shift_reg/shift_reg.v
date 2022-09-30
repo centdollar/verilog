a8a354b1d33a97b89d03570a0a84e4c75bcec95c
centdollar <vincent.michelini1@gmail.com>
2022-09-30 15:19:17 -0400

    added serial in parallel out register file

--- shift_reg/shift_reg.v
+++ shift_reg/shift_reg.v
@@ -4,6 +4,13 @@
 // functionality and paramaterization to make this functionality possible
 //
 // TODO: first get a basic shift register made.
+//       Then make a different shift signal for each possible switch, but
+//       think about maybe using a parameter based shift register 
+//       Lots of options to choose from
+//       parameter ideas:
+//       - width
+//       - shift amount
+//       - idk
 
 
 module shift_reg
@@ -13,6 +20,12 @@ module shift_reg
 (
 
     input [7 : 0] din_i,
+    input shift_i,
+    input clk_i,
 
+    output dout_o,
 
 );
+
+
+endmodule
