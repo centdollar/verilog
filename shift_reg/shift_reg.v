235f7d7efe4cdf057297950e14d0d7338792e8c1
centdollar <vincent.michelini1@gmail.com>
2022-09-30 15:20:02 -0400

    stopped development on this until the SIPO is done

--- shift_reg/shift_reg.v
+++ shift_reg/shift_reg.v
@@ -20,12 +20,27 @@ module shift_reg
 (
 
     input [7 : 0] din_i,
-    input shift_i,
+    input shift_i,          // 1: shift     0: no shift
     input clk_i,
+    input reset_i,          // 1: reset     0: no reset
 
-    output dout_o,
+    output reg [7 : 0] dout_o
 
 );
+
+reg [7 : 0] data_reg;       // register that holds the data while shifting
+
+always @(posedge clk_i)
+begin
+    if(reset_i)
+    begin
+        dout_0
+    end
+
+end
+
+
+
 
 
 endmodule
