2948a156eed8b51a01770ef76e82f985d55a5e93
centdollar <vincent.michelini1@gmail.com>
2022-10-02 14:44:48 -0400

    added the start of the filo code and added the gitignore

---
+++ filo_reg/filo_reg.v
@@ -0,0 +1,14 @@
+// Author: Vincent Michelini
+// Description: verilog code for the FILO register, this will act as a stack
+//              in future uses of this. Due to this the write and read enable
+//              will be called push and pop respectively
+
+
+module filo_reg
+#(
+    parameter DEPTH = 8,
+    parameter WIDTH = 8
+)
+(
+    
+);
